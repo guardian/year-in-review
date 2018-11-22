3e454ea5b1d02e8d2885fe613a010eef68273454
Susie Coleman <susiecoleman20@gmail.com>
2018-11-22 17:47:13 +0000

    Adding welcome intent with placeholder audio.

--- functions/src/content/welcomeContent.ts
+++ functions/src/content/welcomeContent.ts
@@ -1,4 +1,18 @@
 const welcomeAudioLink =
   'https://s3-eu-west-1.amazonaws.com/voicelab-assets/00.intro.mp3';
 
-export { welcomeAudioLink };
+const askAgainAudioLink =
+  'https://actions.google.com/sounds/v1/alarms/dinner_bell_triangle.ogg';
+
+const startYearInReviewAudioLink =
+  'https://actions.google.com/sounds/v1/weather/afternoon_storm_long.ogg';
+
+const leaveYearInReviewAudioLink =
+  'https://actions.google.com/sounds/v1/alarms/mechanical_clock_ring.ogg';
+
+export {
+  welcomeAudioLink,
+  askAgainAudioLink,
+  startYearInReviewAudioLink,
+  leaveYearInReviewAudioLink,
+};
--- functions/src/responses/welcomeResponse.ts
+++ functions/src/responses/welcomeResponse.ts
@@ -1,10 +1,38 @@
+import {
+  askAgainAudioLink,
+  leaveYearInReviewAudioLink,
+  startYearInReviewAudioLink,
+  welcomeAudioLink,
+} from '../content/welcomeContent';
 import { renderXml, ssml } from 'fluent-ssml';
 
-import { welcomeAudioLink } from '../content/welcomeContent';
 const welcomeResponse = renderXml(
   ssml()
     .audio(welcomeAudioLink)
     .render()
 );
 
-export { welcomeResponse };
+const askAgainResponse = renderXml(
+  ssml()
+    .audio(askAgainAudioLink)
+    .render()
+);
+
+const startYearInReviewResponse = renderXml(
+  ssml()
+    .audio(startYearInReviewAudioLink)
+    .render()
+);
+
+const leaveYearInReviewResponse = renderXml(
+  ssml()
+    .audio(leaveYearInReviewAudioLink)
+    .render()
+);
+
+export {
+  welcomeResponse,
+  askAgainResponse,
+  startYearInReviewResponse,
+  leaveYearInReviewResponse,
+};
