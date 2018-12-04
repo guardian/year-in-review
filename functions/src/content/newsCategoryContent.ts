f23cc95f813aa9e076cf3277602ddda3275bca5a
Susie Coleman <susiecoleman20@gmail.com>
2018-12-04 16:39:12 +0000

    At the end of a category play a teaser clip.

--- functions/src/content/newsCategoryContent.ts
+++ functions/src/content/newsCategoryContent.ts
@@ -23,6 +23,9 @@ const newsQuestions: Question[] = [
   ),
 ];
 
-const newsCategory = () => new Category(newsQuestions);
+const newsPodcastTeaser =
+  'https://s3.eu-west-2.amazonaws.com/year-in-focus-audio/teaser.mp3';
+
+const newsCategory = () => new Category(newsQuestions, newsPodcastTeaser);
 
 export { newsCategory };
