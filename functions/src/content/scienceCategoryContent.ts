cba8784cf7108a8d978bb7d43b3e8be629feaca2
susiecoleman <susiecoleman20@gmail.com>
2018-12-06 14:33:22 +0000

    Add science audio.

--- functions/src/content/scienceCategoryContent.ts
+++ functions/src/content/scienceCategoryContent.ts
@@ -10,28 +10,28 @@ import { Category } from '../models/categories';
 
 const scienceQuestions: Question[] = [
   new MultipleChoiceQuestion(
-    'https://s3.eu-west-2.amazonaws.com/year-in-focus-audio/techOpener.mp3',
-    MultipleChoice.C,
-    'https://s3.eu-west-2.amazonaws.com/year-in-focus-audio/techQ1Incorrect.mp3',
-    'https://s3.eu-west-2.amazonaws.com/year-in-focus-audio/techQ1Incorrect.mp3',
-    'https://s3.eu-west-2.amazonaws.com/year-in-focus-audio/techQ1Correct.mp3',
-    'https://s3.eu-west-2.amazonaws.com/year-in-focus-audio/techQ1Incorrect.mp3'
+    'https://storage.googleapis.com/audio-assets/03.1_YiR_Science_Q1.ogg',
+    MultipleChoice.A,
+    'https://storage.googleapis.com/audio-assets/03.1_YiR_Science_Q1b_A.ogg',
+    'https://storage.googleapis.com/audio-assets/03.1_YiR_Science_Q1b_B.ogg',
+    'https://storage.googleapis.com/audio-assets/03.1_YiR_Science_Q1b_C.ogg',
+    'https://storage.googleapis.com/audio-assets/03.1_YiR_Science_Q1b_D.ogg'
   ),
   new FillInTheBlankQuestion(
-    'https://s3.eu-west-2.amazonaws.com/year-in-focus-audio/techQ2.mp3',
+    'https://storage.googleapis.com/audio-assets/03.2_YiR_Science_Q2a.ogg',
     'CRISPR',
-    'https://s3.eu-west-2.amazonaws.com/year-in-focus-audio/techQ2Correct.mp3',
-    'https://s3.eu-west-2.amazonaws.com/year-in-focus-audio/techQ2Incorrect.mp3'
+    'https://storage.googleapis.com/audio-assets/03.2_YiR_Science_Q2b_True.ogg',
+    'https://storage.googleapis.com/audio-assets/03.2_YiR_Science_Q2b_False.ogg'
   ),
   new TrueFalseQuestion(
-    'https://s3.eu-west-2.amazonaws.com/year-in-focus-audio/techQ2.mp3',
+    'https://storage.googleapis.com/audio-assets/03.3_YiR_Science_Q3.ogg',
     true,
-    'https://s3.eu-west-2.amazonaws.com/year-in-focus-audio/techQ2Correct.mp3',
-    'https://s3.eu-west-2.amazonaws.com/year-in-focus-audio/techQ2Incorrect.mp3'
+    'https://storage.googleapis.com/audio-assets/03.3_YiR_Science_Q3b_True.ogg',
+    'https://storage.googleapis.com/audio-assets/03.3_YiR_Science_Q3b_False.ogg'
   ),
 ];
 
-const scienceOpening = ''
+const scienceOpening = 'https://storage.googleapis.com/audio-assets/03.0_YiR_Science_a.ogg'
 
 const scienceCategory = () =>
   new Category(scienceQuestions, scienceOpening);
