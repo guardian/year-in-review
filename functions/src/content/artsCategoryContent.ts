91e38398f7e55af1cd5787fb4804a004cae023af
susiecoleman <susiecoleman20@gmail.com>
2018-12-14 14:26:58 +0000

    Update audio links.

--- functions/src/content/artsCategoryContent.ts
+++ functions/src/content/artsCategoryContent.ts
@@ -12,14 +12,14 @@ const artsQuestions: Question[] = [
   new FillInTheBlankQuestion(
     'https://storage.googleapis.com/audio-assets/04.1_YiR_Arts_Q1.ogg',
     'GAMBINO',
-    'https://storage.googleapis.com/audio-assets/04.1_YiR_Arts_Q1_Correct.ogg',
-    'https://storage.googleapis.com/audio-assets/04.1_YiR_Arts_Q1_Wrong.ogg'
+    'https://storage.googleapis.com/audio-assets/04.2_YiR_Arts_Q1_Correct.ogg',
+    'https://storage.googleapis.com/audio-assets/04.2_YiR_Arts_Q1_Wrong.ogg'
   ),
   new TrueFalseQuestion(
     'https://storage.googleapis.com/audio-assets/04.2_YiR_Arts_Q2.ogg',
     false,
-    'https://storage.googleapis.com/audio-assets/04.2_YiR_Arts_Q2_False.ogg',
-    'https://storage.googleapis.com/audio-assets/04.2_YiR_Arts_Q2_True.ogg'
+    'https://storage.googleapis.com/audio-assets/04.3_YiR_Arts_Q2_False.ogg',
+    'https://storage.googleapis.com/audio-assets/04.3_YiR_Arts_Q2_True.ogg'
   ),
   new MultipleChoiceQuestion(
     'https://storage.googleapis.com/audio-assets/04.3_YiR_Arts_Q3.ogg',
